b4891da44b3f212b4c454899dd23328197455b0c
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-09-16 16:50:44 +0600

    [lint]: using prettier ts file;

---
+++ .prettierrc.ts
@@ -0,0 +1,14 @@
+const config = {
+  singleQuote: true,
+  semi: false,
+  trailingComma: 'es5' as const,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid' as const,
+  endOfLine: 'lf' as const,
+};
+
+export default config;
